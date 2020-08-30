e0c47c2b57d854498ebab75d691264c1b1d02291
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-30 22:55:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  batch = %w()
+  names.each{ |name| batch << badge_maker(name)}
+  batch
+end
+
+def assign_rooms(assignments)
+  room = [1,2,3,4,5,6,7]
+  person = []
+  assignments.each do |name|
+    person << "Hello, #{name}! You'll be assigned to room #{assignments.index(name) + 1}!"
+  end
+  person
+end
+
+def printer(attendees)
+print batch
+print person
+end
