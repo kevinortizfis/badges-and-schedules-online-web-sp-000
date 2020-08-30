33c6a859f2e035b545baf771369a61df38148d83
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-30 23:00:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,6 @@ def assign_rooms(assignments)
 end
 
 def printer(attendees)
-puts (batch_badge_creator(attendees.index)).index
+puts (batch_badge_creator(attendees.collect))
 puts assign_rooms(attendees)
 end
